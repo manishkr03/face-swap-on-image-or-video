15c04ad31f5a221a0d7a69e7e11768f9d6c035fa
Manish Kumar <78295361+manishkr03@users.noreply.github.com>
2021-02-02 03:35:06 +0530

    Add files via upload

---
+++ scripts/faceswap.sh
@@ -0,0 +1,4 @@
+python main.py --src imgs/test6.jpg --dst imgs/test4.jpg --out results/output6_4.jpg --correct_color
+
+or in windows:
+python main.py --src "E:\ML-DL-CV-NLP\DL-CV-Project\FaceSwap\imgs\test6.jpg" --dst "E:\ML-DL-CV-NLP\DL-CV-Project\FaceSwap\imgs\test7.jpg" --out "E:\ML-DL-CV-NLP\DL-CV-Project\FaceSwap\imgs\img67_out.jpg"
